ae5442461d01286b1bfda645ce4fa25f34ea9fb7
Charles Tripp <charles.tripp@nrel.gov>
2021-01-13 10:07:20 -0700

    + added srundmp.sh

---
+++ srundmp.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#SBATCH --job-name=dmp
+#SBATCH --cpus-per-task=8
+#SBATCH --mem-per-cpu=1GB
+#SBATCH --nodes=1
+#SBATCH --tasks-per-node=1
+#SBATCH --time=120
+#SBATCH --account=dmpapps
+#SBATCH --cpu-freq=high-high:Performance
+
+# MODIFY HERE according to your environment setup
+source ~/admp
+unset LD_PRELOAD
+
+code
+
+echo "executing command... python -u -m "$@"
+
+python -u -m "$@"
+
