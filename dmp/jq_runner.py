0970770a8599d714e5f67fe3e9551ab04ef9d8de
Jordan Perr-Sauer <jordan.perr-sauer@nrel.gov>
2021-04-16 10:40:01 -0600

    turned off subprocess pipe capture, since this could cause deadlock with large output

--- dmp/jq_runner.py
+++ dmp/jq_runner.py
@@ -9,25 +9,23 @@ if __name__ == "__main__":
     jq = jobqueue.JobQueue(sys.argv[1], sys.argv[2])
 
     while True:
-        time.sleep(0.1)
         message = jq.get_message()
         if message is None:
+            time.sleep(5)
             continue
         try:
-            print(f"{message.uuid} RUNNING")
+            print(f"Job Queue: {message.uuid} RUNNING")
             config_module = message.config["jq_module"]
             message.config["jq_uuid"] = message.uuid
             config_str = json.dumps(message.config)
-            result = subprocess.run(["python", "-u", "-m", config_module, config_str], capture_output=True)
+            result = subprocess.run(["python", "-u", "-m", config_module, config_str])
             if result.returncode != 0:
-                print(f"{message.uuid} exited with bad retun code: {result.returncode}")
-                print(f"-- stdout: {result.stdout}")
-                print(f"-- stderr: {result.stderr}")
+                print(f"Job Queue: {message.uuid} exited with bad retun code: {result.returncode}")
             else:
                 message.mark_complete()
-                print(f"{message.uuid} DONE")
+                print(f"Job Queue: {message.uuid} DONE")
         except Exception as e:
-            print(f"Unknown exception in jq_runner: {e}")
+            print(f"Job Queue: Unknown exception in jq_runner: {e}")
 
 
 # SELECT * FROM jobqueue WHERE groupname='test_tag';
