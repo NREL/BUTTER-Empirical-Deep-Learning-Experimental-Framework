c86f49087c989c4c80fc7ff4082f4e5e3aab8760
Charles Tripp <charles.tripp@nrel.gov>
2021-06-15 17:41:11 -0600

    + fixed some lingering bugs in new worker dispatch flow

--- dmp/jobqueue/jq_worker.py
+++ dmp/jobqueue/jq_worker.py
@@ -11,9 +11,10 @@ import sys
 import os
 
 
-def run_worker(strategy, project, group, max_waiting_time=10 * 60):
+def run_worker(strategy, config, project, group, max_waiting_time=10 * 60):
     print(f"Job Queue: Starting...")
 
+    # with tensorflow.Session(config=config):
     jq = jobqueue.JobQueue(project, group)
     wait_start = None
     while True:
@@ -58,40 +59,49 @@ def run_worker(strategy, project, group, max_waiting_time=10 * 60):
             print(f"Job Queue: {message.uuid} Unknown exception in jq_runner: {e}")
 
 
-def tf_gpu_session():
-    """
-    Return a tensorflow session with the configuration we'll use on a GPU node.
-    """
-    config = tensorflow.ConfigProto()
-    # config.gpu_options.allow_growth = False
-    # config.gpu_options.per_process_gpu_memory_fraction = 0.2
-    return tensorflow.Session(config=config)
+# def tf_gpu_session():
+#     """
+#     Return a tensorflow session with the configuration we'll use on a GPU node.
+#     """
+#     config = tensorflow.ConfigProto()
+#     config.gpu_options.allow_growth = True
+#     config.gpu_options.per_process_gpu_memory_fraction = .5
+#     return tensorflow.Session(config=config)
 
 
-def make_strategy(cpu_min, cpu_max, gpu_min, gpu_max):
+def make_strategy(cpu_low, cpu_high, gpu_low, gpu_high):
     # tensorflow.distribute.MirroredStrategy(devices=["/gpu:0", "/gpu:1"])
 
-    num_cpu = cpu_max - cpu_min
-    num_gpu = gpu_max - gpu_min
+    num_cpu = cpu_high - cpu_low
+    num_gpu = gpu_high - gpu_low
 
     devices = []
-    devices.extend(['/GPU:' + str(i) for i in range(gpu_min, gpu_max)])
+    devices.extend(['/GPU:' + str(i) for i in range(gpu_low, gpu_high)])
     if num_cpu > num_gpu * 2:  # no CPU device if 2 or fewer CPUs per GPU
         devices.append('/CPU:0')  # TF batches all CPU's into one device
 
     num_threads = max(1, num_cpu)  # make sure we have one thread even if not using any CPUs
-    tensorflow.config.threading.set_intra_op_parallelism_threads(num_threads)
-    tensorflow.config.threading.set_inter_op_parallelism_threads(num_threads)
+
+    # tensorflow.config.threading.set_intra_op_parallelism_threads(num_threads)
+    # tensorflow.config.threading.set_inter_op_parallelism_threads(num_threads)
+    # tensorflow.config.set_visible_devices(devices)
+
+    # gpu_options = tensorflow.GPUOptions(per_process_gpu_memory_fraction=0.995)
+    # cpu_options = tensorflow.CPU
+    # config = tensorflow.ConfigProto(gpu_options=gpu_options)
+
+    print(f'num_cpu {num_cpu}, num_gpu {num_gpu}, num_threads {num_threads}')
 
     if len(devices) == 1:
+        # if num_gpu > 1:
         strategy = tensorflow.distribute.OneDeviceStrategy(device=devices[0])
+        # else:
+        #     strategy = tensorflow.distribute.get_strategy()  # the default strategy
     else:
-        strategy = tensorflow.distribute.MirroredStrategy(
-            devices=devices,
-            cross_device_ops=tensorflow.contrib.distribute.AllReduceCrossDeviceOps(all_reduce_alg="hierarchical_copy"))
+        strategy = tensorflow.distribute.MirroredStrategy(devices=devices)
 
     print('num_replicas_in_sync: {}'.format(strategy.num_replicas_in_sync))
-    return strategy
+    return strategy, None
 
 
 if __name__ == "__main__":
@@ -104,5 +114,5 @@ if __name__ == "__main__":
     parser.add_argument('group', help='group name or tag')
     args = parser.parse_args()
 
-    strategy = make_strategy(args.cpu_low, args.gpu_high, args.gpu_low, args.gpu_high)
-    run_worker(strategy, args.project, args.group)
+    strategy, config = make_strategy(args.cpu_low, args.cpu_high, args.gpu_low, args.gpu_high)
+    run_worker(strategy, config, args.project, args.group)
