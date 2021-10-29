73c6010a01242b22cc21fa31ade44dc3a08955e2
Charles Tripp <charles.tripp@nrel.gov>
2021-10-29 16:37:20 -0600

    + set up jq worker and node manager to (hopefully) forward output

--- dmp/jq/jq_node_manager.py
+++ dmp/jq/jq_node_manager.py
@@ -2,6 +2,10 @@ import argparse
 import json
 import platform
 import subprocess
+import sys
+from asyncio import Queue
+from queue import Empty
+from threading import Thread
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -22,15 +26,48 @@ if __name__ == "__main__":
     print(f'Launching worker processes...')
     print(json.dumps(configs))
 
-    workers = []
-    for rank, config in enumerate(configs):
-        command = ['python', '-m', 'dmp.jq.jq_worker_manager',
-                   'python', '-m', 'dmp.jq.jq_worker',
-                   *[str(e) for e in config], args.project, args.group]
-        print(f'Creating subprocess with command: "{" ".join(command)}"')
-        workers.append(subprocess.Popen(
-            command, bufsize=1, universal_newlines=True, stdout=subprocess.STDOUT, stderr=subprocess.STDOUT))
-    print(f'Waiting for worker processes to exit...')
-    for worker in workers:
-        worker.wait()
-    print('Exiting Worker Manager...')
+
+    def enqueue_output(out, queue):
+        for line in iter(out.readline, b''):
+            queue.put(line)
+        out.close()
+
+
+    if __name__ == '__main__':
+        logfile = open('logfile.txt', 'w')
+
+        workers = []
+        for rank, config in enumerate(configs):
+            command = ['python', '-m', 'dmp.jq.jq_worker_manager',
+                       'python', '-m', 'dmp.jq.jq_worker',
+                       *[str(e) for e in config], args.project, args.group]
+            print(f'Creating subprocess with command: "{" ".join(command)}"')
+            workers.append(subprocess.Popen(
+                command, bufsize=1, universal_newlines=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE))
+
+        q = Queue()
+        threads = []
+        for w in workers:
+            t = Thread(target=enqueue_output, args=(w.stdout, q))
+            threads.append(t)
+            t.daemon = True
+            t.start()
+
+        while True:
+            try:
+                line = q.get_nowait()
+            except Empty:
+                pass
+            else:
+                sys.stdout.write(line)
+                logfile.write(line)
+                logfile.flush()
+
+            # break when all processes are done.
+            if all(w.poll() is not None for w in workers):
+                break
+
+        print(f'Waiting for worker processes to exit...')
+        for worker in workers:
+            worker.wait()
+        print('Exiting Worker Manager...')
