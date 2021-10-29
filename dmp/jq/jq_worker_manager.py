497d9c32963d6750f40763b76295438e2da4ca43
Charles Tripp <charles.tripp@nrel.gov>
2021-10-29 16:11:33 -0600

    + set up jq worker and node manager to (hopefully) forward output

--- dmp/jq/jq_worker_manager.py
+++ dmp/jq/jq_worker_manager.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
         completed_process = subprocess.run(subprocess_args,
                                            capture_output=False,
                                            bufsize=0,
-                                           # universal_newlines=True,
+                                           universal_newlines=True,
                                            stdout=subprocess.STDOUT,
                                            stderr=subprocess.STDOUT)
         if completed_process.returncode != 1:
