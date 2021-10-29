3fbe20e02cab2985a0d07ba97a9e3df7950a21c4
Charles Tripp <charles.tripp@nrel.gov>
2021-10-29 16:17:38 -0600

    + set up jq worker and node manager to (hopefully) forward output

--- dmp/jq/jq_worker_manager.py
+++ dmp/jq/jq_worker_manager.py
@@ -8,13 +8,15 @@ if __name__ == "__main__":
     print(f'Starting Worker Manager...')
     while True:
         print(f'Launching subprocess command "{" ".join(subprocess_args)}"...')
-        print('asdf...')
-        completed_process = subprocess.run(subprocess_args,
-                                           capture_output=False,
-                                           bufsize=0,
-                                           universal_newlines=True,
-                                           stdout=subprocess.STDOUT,
-                                           stderr=subprocess.STDOUT)
+        # completed_process = subprocess.run(subprocess_args,
+        #                                    # capture_output=False,
+        #                                    bufsize=0,
+        #                                    # universal_newlines=True,
+        #                                    stdout=subprocess.STDOUT,
+        #                                    stderr=subprocess.STDOUT)
+        completed_process = subprocess.Popen(subprocess_args,
+                                             bufsize=1, universal_newlines=True, stdout=subprocess.STDOUT,
+                                             stderr=subprocess.STDOUT)
         if completed_process.returncode != 1:
             break
         print(f'Subprocess failed with returncode {completed_process.returncode}.')
