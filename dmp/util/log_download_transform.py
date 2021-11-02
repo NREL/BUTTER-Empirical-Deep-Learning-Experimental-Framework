b5ce3092a1c08d1ac2ffff7c1e77df8b9d33fa1f
Charles Tripp <charles.tripp@nrel.gov>
2021-11-02 17:15:40 -0600

    + added script to download log data

---
+++ dmp/util/log_download_transform.py
@@ -0,0 +1,296 @@
+import sys
+
+import numpy
+
+sys.path.append("../../")
+
+import pandas as pd
+import gc
+import time
+import os
+import json
+import sqlalchemy
+
+_database = "dmp"
+try:
+    filename = os.path.join(os.environ['HOME'], ".jobqueue.json")
+    _data = json.loads(open(filename).read())
+    _credentials = _data[_database]
+    user = _credentials["user"]
+except KeyError as e:
+    raise Exception("No credetials for {} found in {}".format(_database, filename))
+connection_string = 'postgresql://{user}:{password}@{host}:5432/{database}'.format(**_credentials)
+# connection_string = 'postgresql+asyncpg://{user}:{password}@{host}:5432/{database}'.format(**_credentials)
+del _credentials['table_name']
+
+from pathos.multiprocessing import Pool
+import math
+
+# async def fetch_as_dataframe(con: asyncpg.Connection, query: str, *args):
+#     stmt = await con.prepare(query)
+#     columns = [a.name for a in stmt.get_attributes()]
+#     data = await stmt.fetch(*args)
+#     return pandas.DataFrame(data, columns=columns)
+
+
+drop_list = [
+        'loss', 'task', 'endpoint', 'run_name', 'config.run_name', 'val_loss', 'iterations',
+        'num_inputs', 'num_classes', 'num_outputs', 'num_weights',
+        'num_features', 'num_observations', 'config.log', 'config.rep',
+        'config.mode', 'config.name', 'config.reps', 'config.seed',
+        'config.depths', 'config.widths',
+        'config.budgets', 'config.datasets',
+        'config.jq_module',
+        'config.run_config.shuffle', 'config.run_config.verbose',
+        'config.topologies', 'config.epoch_scale.b', 'config.epoch_scale.m',
+        'config.label_noises',
+        'config.early_stopping', 'config.learning_rates',
+        'config.residual_modes', 'config.network_structure',
+        'config.validation_split_method',
+        'environment.git_hash',
+        'environment.hostname', 'environment.platform',
+        'environment.SLURM_JOB_ID', 'environment.python_version',
+        'environment.tensorflow_version',
+        'config.num_hidden'
+]
+
+rename_map = {
+    s: s[len('history.'):] for s in [
+        'history.loss', 'history.hinge',
+        'history.accuracy', 'history.val_loss', 'history.val_hinge',
+        'history.val_accuracy', 'history.squared_hinge',
+        'history.cosine_similarity', 'history.val_squared_hinge',
+        'history.mean_squared_error', 'history.mean_absolute_error',
+        'history.val_cosine_similarity', 'history.val_mean_squared_error',
+        'history.root_mean_squared_error', 'history.val_mean_absolute_error',
+        'history.kullback_leibler_divergence',
+        'history.val_root_mean_squared_error',
+        'history.mean_squared_logarithmic_error',
+        'history.val_kullback_leibler_divergence',
+        'history.val_mean_squared_logarithmic_error',
+    ]}
+
+rename_map.update({
+    s: s[len('config.'):] for s in
+    [
+        'config.mode', 'config.name', 'config.reps', 'config.seed',
+        'config.depth', 'config.budget', 'config.depths', 'config.widths',
+        'config.budgets', 'config.dataset', 'config.datasets',
+        'config.run_name', 'config.topology', 'config.jq_module',
+        'config.optimizer.config.learning_rate', 'config.optimizer.class_name',
+        'config.activation', 'config.num_hidden', 'config.run_config.epochs',
+        'config.run_config.shuffle', 'config.run_config.verbose',
+        'config.run_config.batch_size', 'config.run_config.validation_split',
+        'config.topologies', 'config.epoch_scale.b', 'config.epoch_scale.m',
+        'config.label_noise', 'config.label_noises', 'config.residual_mode',
+        'config.early_stopping', 'config.learning_rates',
+        'config.residual_modes', 'config.network_structure',
+        'config.validation_split_method',
+    ]
+})
+for k in drop_list:
+    rename_map.pop(k, None)
+
+type_map = {
+'depth' : 'int16',
+'budget' : 'int64',
+'dataset' : 'str',
+'topology' : 'str',
+'optimizer.config.learning_rate' : 'float32',
+'optimizer.class_name' : 'str',
+'activation' : 'str',
+'run_config.epochs' : 'int32',
+'run_config.batch_size' : 'int32',
+'run_config.validation_split' :'float32',
+# 'label_noise' : 'float32',
+'residual_mode' : 'str',
+# 'loss' : 'float32[]',
+# 'hinge' : 'float32[]',
+# 'accuracy' : 'float32[]',
+# 'val_loss' : 'float32[]',
+# 'val_hinge' : 'float32[]',
+# 'val_accuracy' : 'float32[]',
+# 'squared_hinge' : 'float32[]',
+# 'cosine_similarity' : 'float32[]',
+# 'val_squared_hinge' : 'float32[]',
+# 'mean_squared_error' : 'float32[]',
+# 'mean_absolute_error' : 'float32[]',
+# 'val_cosine_similarity' : 'float32[]',
+# 'val_mean_squared_error' : 'float32[]',
+# 'root_mean_squared_error' : 'float32[]',
+# 'val_mean_absolute_error' : 'float32[]',
+# 'kullback_leibler_divergence' : 'float32[]',
+# 'val_root_mean_squared_error' : 'float32[]',
+# 'mean_squared_logarithmic_error' : 'float32[]',
+# 'val_kullback_leibler_divergence' : 'float32[]',
+# 'val_mean_squared_logarithmic_error' : 'float32[]',
+'id' : 'int32',
+'job' : 'str',
+}
+
+array_cols = [
+'loss',
+'hinge',
+'accuracy',
+'val_loss',
+'val_hinge',
+'val_accuracy',
+'squared_hinge',
+'cosine_similarity',
+'val_squared_hinge',
+'mean_squared_error',
+'mean_absolute_error',
+'val_cosine_similarity',
+'val_mean_squared_error',
+'root_mean_squared_error',
+'val_mean_absolute_error',
+'kullback_leibler_divergence',
+'val_root_mean_squared_error',
+'mean_squared_logarithmic_error',
+'val_kullback_leibler_divergence',
+'val_mean_squared_logarithmic_error',
+]
+
+
+def postprocess_dataframe(data_log):
+    # print(f'{len(data_log)} records retrieved. Parsing json...')
+    datasets = pd.json_normalize(data_log["doc"])
+    data_log.drop(columns=['doc'], inplace=True)
+    #     datasets = pd.json_normalize(data_log['doc'].map(orjson.loads))
+    # print(datasets.columns)
+    datasets.drop(columns=drop_list, inplace=True)
+    # print(datasets.columns)
+    datasets.rename(columns=rename_map, inplace=True)
+    # print(datasets.columns)
+    # print(f'Joining with original dataframe...')
+    datasets = datasets.join(data_log)
+    # print(datasets.columns)
+    # print(datasets.dtypes)
+    # print(f'converting...')
+    datasets = datasets.astype(type_map)
+
+    def convert_label_noise(v):
+        try:
+            float_value = float(v)
+        except ValueError:
+            return 0.0
+    datasets['label_noise'] = datasets['label_noise'].apply(convert_label_noise)
+    
+
+    #
+    # datasets['id'] = datasets.astype({'id': 'str'}).dtypes
+    # datasets['job'] = datasets.astype({'job': 'str'}).dtypes
+
+    for col in array_cols:
+        datasets[col] = datasets[col].apply(lambda e : numpy.array(e, dtype=numpy.single))
+
+    # print(datasets.dtypes)
+    return datasets
+
+
+def func():
+    # log_filename = 'aspect_analysis_datasets.feather'
+    log_filename = 'fixed_3k_1.parquet'
+    groupname = 'fixed_3k_1'
+    # engine, session = log._connect()
+
+    #     datasets = None
+    #     min_time =  datetime.datetime.fromisoformat('2000-01-01 00:00:00')
+    #     try:
+    #     #     datasets = pd.read_feather(log_filename)
+    #         datasets = pd.read_parquet(log_filename)
+    #         min_time = datasets['timestamp'].min()
+    #     except:
+    #         print(f'Error reading dataset file, {log_filename}.')
+
+    conditions = f'log.groupname = \'{groupname}\''
+    q = f'select count(*) from "log" where {conditions}'
+
+    db = sqlalchemy.create_engine(connection_string)
+    engine = db.connect()
+    count = db.engine.execute(q).scalar()
+    engine.close()
+
+    loaded = 0
+    chunk_size = 16
+    max_buffered_chunks = 256
+    print(f'Loading {count} records from database...')
+
+    # num_readers = min(12, int(math.ceil(count / chunk_size)))
+    num_readers = min(64, int(math.ceil((count/chunk_size))))
+    read_size = int(math.ceil(count / num_readers))
+
+    print(f'Reading {count} records with read_size {read_size} and {num_readers} readers...')
+
+    def read_chunk(read_number):
+        print(f'Read #{read_number}: starting...')
+        db = sqlalchemy.create_engine(connection_string)
+        engine = db.connect()
+        print(f'Read #{read_number}: connected')
+        q = f'''
+        SELECT
+        log.id as id,
+        log.job as job,
+        log.timestamp as timestamp,
+        (jobqueue.end_time - jobqueue.start_time) AS job_length,
+        log.doc as doc
+        FROM
+             log,
+             jobqueue
+        WHERE
+            {conditions} AND
+            jobqueue.uuid = log.job
+        ORDER BY id ASC
+        LIMIT {read_size} OFFSET {read_size * read_number}
+        '''
+        data_generator = pd.read_sql(
+            q,
+            engine.execution_options(stream_results=True), coerce_float=False, chunksize=chunk_size,
+            params=())
+
+        print(f'Read #{read_number}: begin read')
+        chunks = []
+        for chunk in data_generator:
+            num_entries = len(chunk)
+            print(f'Read #{read_number}: processing chunk size {num_entries} from database...')
+            chunk = postprocess_dataframe(chunk)
+            chunks.append(chunk)
+            if len(chunks) >= max_buffered_chunks:
+                chunks = [pd.concat(chunks)]
+                gc.collect()
+
+        results = pd.concat(chunks)
+        print(f'Read #{read_number}: complete, read {len(results)} results.')
+        return results
+
+    print(f'Initializing pool...')
+    start_time = time.perf_counter()
+    # for i in range(0, num_readers):
+    #     read_chunk(i)
+    with Pool(num_readers) as p:
+        data_load = p.map(read_chunk, range(0, num_readers))
+    # data_load = Parallel(n_jobs=num_readers, batch_size=1, backend='multiprocessing')(
+    #     delayed(read_chunk(i)) for i in range(num_readers))
+
+    gc.collect()
+    datasets = pd.concat(data_load)
+    delta_t = time.perf_counter() - start_time
+    num_entries = len(datasets)
+    print(f'Processed {num_entries} entries in {delta_t}s at a rate of {num_entries / delta_t} entries / second.')
+
+    if loaded > 0:
+        print(f'Loaded {len(datasets)} entries from the database. Saving dataframe to disk...')
+        datasets.to_parquet(log_filename)
+        print(f'Write complete.')
+
+    #     print(f'A total of {len(datasets)} records loaded.')
+
+    data_log = None
+    additional_datasets = None
+    additional_data_log = None
+    # log._close(engine, session)
+
+    gc.collect()
+
+
+func()
