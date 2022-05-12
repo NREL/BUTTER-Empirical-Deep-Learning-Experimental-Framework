e2df98a61d059f764b34c76cd34e7d64004c22d6
ctripp <charles.tripp@nrel.gov>
2022-05-12 12:16:41 -0600

    + 3 worker / gpu max

--- dmp/jq/jq_node_manager.py
+++ dmp/jq/jq_node_manager.py
@@ -71,7 +71,7 @@ def main():
     min_total_worker_gpu_mem = min_gpu_mem_per_worker + worker_gpu_mem_overhead
 
     min_gpu_mem_buffer = 500
-    max_worker_per_gpu = 2
+    max_worker_per_gpu = 3
 
     cores_per_gpu_worker = 1
     min_cores_per_cpu_worker = 1
