fc4d488ba61b83ff3530b3d75bcadc551ce14036
ctripp <charles.tripp@nrel.gov>
2022-05-13 14:57:49 -0600

    + updated run scripts

--- gpu_run_script.sh
+++ gpu_run_script.sh
@@ -15,23 +15,21 @@ echo "$socket_list"
 echo "$core_list"
 
 
-export OMP_DYNAMIC=false
-export OMP_WAIT_POLICY=active
-export OMP_PROC_BIND=true
-export OMP_PLACES=cores
-export OMP_NUM_THREADS=$(num_cores)
+#export OMP_DYNAMIC=false
+#export OMP_WAIT_POLICY=active
+#export OMP_PROC_BIND=true
+#export OMP_PLACES=cores
+export OMP_NUM_THREADS=$((num_cores))
 export OMP_DISPLAY_AFFINITY=TRUE
 export KMP_AFFINITY=verbose
-export CRAY_OMP_CHECK_AFFINITY=TRUE
+#export CRAY_OMP_CHECK_AFFINITY=TRUE
 
 export TF_ENABLE_MKL_NATIVE_FORMAT=0
 export TF_ENABLE_ONEDNN_OPTS=1
-export KMP_AFFINITY=granularity=fine,compact,0,1
-export KMP_BLOCKTIME=10
+export KMP_AFFINITY=verbose,warnings,granularity=fine,compact,0,1
+export KMP_BLOCKTIME=1
 export KMP_SETTINGS=TRUE
 export TF_ENABLE_XLA=1
-#export OMP_NUM_THREADS=18
-
 
 # export ZENDNN_MEMPOOL_ENABLE=1
 # export ZENDNN_TENSOR_POOL_LIMIT=16
@@ -39,5 +37,4 @@ export TF_ENABLE_XLA=1
 # export ZENDNN_BLOCKED_FORMAT=0
 # export ZENDNN_PRIMITIVE_CACHE_CAPACITY=1024
 
-echo "Executing: " numactl --cpunodebind="$socket_list" --preferred="$socket_list" --physcpubind="$core_list" "${command[@]}"
-numactl --cpunodebind="$socket_list" --preferred="$socket_list" --physcpubind="$core_list" "${command[@]}"
+numactl --cpunodebind="$socket_list" --membind="$socket_list" --physcpubind="$core_list" --show "${command[@]}"
