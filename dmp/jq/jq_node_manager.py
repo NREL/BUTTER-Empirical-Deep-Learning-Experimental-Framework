d34d783c357c639940bf022f1c48eb69ba4a90f8
ctripp <charles.tripp@nrel.gov>
2022-05-18 08:57:43 -0600

    + another little fix

--- dmp/jq/jq_node_manager.py
+++ dmp/jq/jq_node_manager.py
@@ -34,7 +34,7 @@ def run_worker(run_script, project, queue, workers, config):
         num_nodes, num_cores, node_list, core_list,
         'python', '-u', '-m', 'dmp.jobqueue_interface.worker_manager',
         'python', '-u', '-m', 'dmp.jobqueue_interface.worker',
-        node_list[0], num_nodes, core_list[0], num_cores, config[2], config[3], config[4], project, queue]
+        nodes[0], num_nodes, cores[0], num_cores, config[2], config[3], config[4], project, queue]
     return make_worker_process(len(workers), command)
 
 
