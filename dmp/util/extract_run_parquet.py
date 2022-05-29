8cb6988cb178cb5af6b838985b8e536f34d27e7f
ctripp <charles.tripp@nrel.gov>
2022-05-29 14:31:34 -0600

    + more run extractor workers

--- dmp/util/extract_run_parquet.py
+++ dmp/util/extract_run_parquet.py
@@ -298,7 +298,7 @@ def main():
     results = None
 
     num_stored = 0
-    with multiprocessing.ProcessPool(1) as pool:
+    with multiprocessing.ProcessPool(38) as pool:
         results = pool.uimap(download_chunk, chunks)
         for num_rows, chunk in results:
             num_stored += 1
