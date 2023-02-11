39c804420781af38ea5f381d07105de80665c7e9
ctripp <charles.tripp@nrel.gov>
2023-02-11 16:13:14 -0700

    + wip

--- dmp/postgres_interface/update_experiment_summary.py
+++ dmp/postgres_interface/update_experiment_summary.py
@@ -37,7 +37,7 @@ class UpdateExperimentSummary(Task):
 
     def __call__(self, worker: Worker, job: Job) -> TaskResult:
         num_summaries = 0
-        experiment_limit = 16
+        experiment_limit = 64
 
         schema = worker._schema
         experiment = schema.experiment
