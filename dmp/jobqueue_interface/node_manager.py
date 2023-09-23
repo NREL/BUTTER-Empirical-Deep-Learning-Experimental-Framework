2e1b4509797acd98e793db162bf78c4b4305c0da
CharlesEdisonTripp <charles.tripp@nrel.gov>
2023-09-23 01:16:03 -0600

    + min_cpus_per_worker = 14

--- dmp/jobqueue_interface/node_manager.py
+++ dmp/jobqueue_interface/node_manager.py
@@ -88,7 +88,7 @@ def main():
     max_worker_per_gpu = 2
 
     cpus_per_gpu_worker = 1
-    min_cpus_per_cpu_worker = 8
+    min_cpus_per_cpu_worker = 14
 
     smt_level = 1  # maximum number of SMT's (a.k.a. "CPUs") per core to use
 
