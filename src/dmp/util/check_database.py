11a359172291a36d5bd2de2a0b9ef0c741ef55d2
CharlesEdisonTripp <charles.tripp@nrel.gov>
2024-05-08 13:56:08 -0600

    + update check database with status messages

--- src/dmp/util/check_database.py
+++ src/dmp/util/check_database.py
@@ -26,24 +26,27 @@ def main():
     print(credentials)
     print("Credentials loaded, attempting to connect.")
     connection = jobqueue.connect(credentials)
+    print("Connection object constructed.)
     try:
-        cursor = connection.cursor()
-        cursor.execute(
-            sql.SQL(
-                """
-select
-    queue, min(priority) min_priority, max(priority) max_priority, avg(priority) avg_priority, command->'batch' batch, command->'shape' shape, status, count(*), avg(log((command->'size')::bigint)/log(2))::bigint avg_log_size
-from
-    job_status s,
-    job_data d
-where s.id = d.id and queue = 1
-group by status, queue, batch, shape
-order by status asc, min_priority asc, queue, batch, shape
-;"""
+        with connection.cursor() as cursor:
+            print("Cursor constructed.)
+            cursor.execute(
+                sql.SQL(
+                    """
+    select
+        queue, min(priority) min_priority, max(priority) max_priority, avg(priority) avg_priority, command->'batch' batch, command->'shape' shape, status, count(*), avg(log((command->'size')::bigint)/log(2))::bigint avg_log_size
+    from
+        job_status s,
+        job_data d
+    where s.id = d.id and queue = 1
+    group by status, queue, batch, shape
+    order by status asc, min_priority asc, queue, batch, shape
+    ;"""
+                )
             )
-        )
-        for i in cursor.fetchall():
-            print(i)
+            print("Query Executed.")
+            for i in cursor.fetchall():
+                print(i)
         print("All is well.")
     except Exception as e:
         print("Exception: f{e}")
