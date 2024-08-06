459870361e464aed5b3cdb5baad3ba5494620e1f
CharlesEdisonTripp <charles.tripp@nrel.gov>
2024-08-05 18:14:45 -0600

    + again

--- src/dmp/util/recompress_model_data.py
+++ src/dmp/util/recompress_model_data.py
@@ -12,93 +12,96 @@ from dmp.task.experiment.training_experiment.training_epoch import TrainingEpoch
 
 
 def recompress_model_data_file(filename):
-    src_path = os.path.join(model_serialization.model_data_dir, filename)
-    dst_path = os.path.join(model_serialization.model_data_dir, filename + ".dst")
-    print(f"load: {src_path}")
-    with h5.File(src_path, "r") as src_file:
-        (
-            src_epoch_dataset,
-            src_parameter_dataset,
-            src_optimizer_datasets,
-        ) = model_serialization.get_datasets_from_model_file(src_file, None)
-
-        src_epochs = model_serialization.convert_epoch_dataset_into_epochs(
-            src_epoch_dataset
-        )
-        print(src_epochs)
-        print(f"\n\n")
-
-        retained_map = {}
-        for epoch in src_epochs:
-            if not ((epoch.marker != 0) or (epoch.fit_number == 0)):
-                continue
-            e = dataclasses.replace(epoch)
-            e.sequence_number = None
-            if e in retained_map:
-                if retained_map[e].sequence_number >= epoch.sequence_number:
-                    continue
-            retained_map[e] = epoch
-
-        retained_epochs: List[TrainingEpoch] = sorted(retained_map.values())
-        num_retained_epochs = len(retained_epochs)
-
-        if num_retained_epochs == len(src_epochs):
-            print(f"already minimized: {filename}")
-            return False
-
-        # print(retained_epochs)
-
-        for epoch in retained_epochs:
-            print(epoch)
-
-        print(
-            f"Num retained epochs: {num_retained_epochs} Ratio: {num_retained_epochs / len(src_epochs)}"
-        )
-
-        with h5.File(dst_path, "w") as dst_file:
+    try:
+        src_path = os.path.join(model_serialization.model_data_dir, filename)
+        dst_path = os.path.join(model_serialization.model_data_dir, filename + ".dst")
+        print(f"load: {src_path}")
+        with h5.File(src_path, "r") as src_file:
             (
-                dst_epoch_dataset,
-                dst_parameter_dataset,
-                dst_optimizer_datasets,
-            ) = model_serialization.get_datasets_from_model_file(
-                dst_file, [member for member, dataset in src_optimizer_datasets]
+                src_epoch_dataset,
+                src_parameter_dataset,
+                src_optimizer_datasets,
+            ) = model_serialization.get_datasets_from_model_file(src_file, None)
+
+            src_epochs = model_serialization.convert_epoch_dataset_into_epochs(
+                src_epoch_dataset
+            )
+            print(src_epochs)
+            print(f"\n\n")
+
+            retained_map = {}
+            for epoch in src_epochs:
+                if not ((epoch.marker != 0) or (epoch.fit_number == 0)):
+                    continue
+                e = dataclasses.replace(epoch)
+                e.sequence_number = None
+                if e in retained_map:
+                    if retained_map[e].sequence_number >= epoch.sequence_number:
+                        continue
+                retained_map[e] = epoch
+
+            retained_epochs: List[TrainingEpoch] = sorted(retained_map.values())
+            num_retained_epochs = len(retained_epochs)
+
+            if num_retained_epochs == len(src_epochs):
+                print(f"already minimized: {filename}")
+                return False
+
+            # print(retained_epochs)
+
+            for epoch in retained_epochs:
+                print(epoch)
+
+            print(
+                f"Num retained epochs: {num_retained_epochs} Ratio: {num_retained_epochs / len(src_epochs)}"
             )
 
-            dataset_mapping = [
-                ("epoch_dataset", src_epoch_dataset, dst_epoch_dataset),
-                ("parameter_dataset", src_parameter_dataset, dst_parameter_dataset),
-            ] + [
-                (src_member, src_dataset, dst_dataset)
-                for (src_member, src_dataset), (dst_member, dst_dataset) in zip(
-                    src_optimizer_datasets, dst_optimizer_datasets
+            with h5.File(dst_path, "w") as dst_file:
+                (
+                    dst_epoch_dataset,
+                    dst_parameter_dataset,
+                    dst_optimizer_datasets,
+                ) = model_serialization.get_datasets_from_model_file(
+                    dst_file, [member for member, dataset in src_optimizer_datasets]
                 )
-            ]
 
-            for dataset_name, src_dataset, dst_dataset in dataset_mapping:
-                print(
-                    f"resize {dataset_name} from {src_dataset.shape} to ({src_dataset.shape[0], num_retained_epochs})"
-                )
-                dst_dataset.resize((src_dataset.shape[0], num_retained_epochs))
+                dataset_mapping = [
+                    ("epoch_dataset", src_epoch_dataset, dst_epoch_dataset),
+                    ("parameter_dataset", src_parameter_dataset, dst_parameter_dataset),
+                ] + [
+                    (src_member, src_dataset, dst_dataset)
+                    for (src_member, src_dataset), (dst_member, dst_dataset) in zip(
+                        src_optimizer_datasets, dst_optimizer_datasets
+                    )
+                ]
 
-            for dst_sequence_number, epoch in enumerate(retained_epochs):
-                src_sequence_number = epoch.sequence_number
-                print(f"copying {src_sequence_number} to {dst_sequence_number}.")
-                for datset_name, src_dataset, dst_dataset in dataset_mapping:
-                    print(f"copying {datset_name}...")
-                    dst_dataset[:, dst_sequence_number] = src_dataset[
-                        :, src_sequence_number
-                    ]
-                dst_epoch_dataset[3, dst_sequence_number] = epoch.marker
+                for dataset_name, src_dataset, dst_dataset in dataset_mapping:
+                    print(
+                        f"resize {dataset_name} from {src_dataset.shape} to ({src_dataset.shape[0], num_retained_epochs})"
+                    )
+                    dst_dataset.resize((src_dataset.shape[0], num_retained_epochs))
 
-        print(f"Done writing. Renaming...")
+                for dst_sequence_number, epoch in enumerate(retained_epochs):
+                    src_sequence_number = epoch.sequence_number
+                    print(f"copying {src_sequence_number} to {dst_sequence_number}.")
+                    for datset_name, src_dataset, dst_dataset in dataset_mapping:
+                        print(f"copying {datset_name}...")
+                        dst_dataset[:, dst_sequence_number] = src_dataset[
+                            :, src_sequence_number
+                        ]
+                    dst_epoch_dataset[3, dst_sequence_number] = epoch.marker
 
-        delete_path = src_path + ".del"
-        subprocess.run(["mv", src_path, delete_path])
-        subprocess.run(["mv", dst_path, src_path])
-        subprocess.run(["rm", delete_path])
+            print(f"Done writing. Renaming...")
 
-        print(f"Done {filename}.")
-        return True
+            delete_path = src_path + ".del"
+            subprocess.run(["mv", src_path, delete_path])
+            subprocess.run(["mv", dst_path, src_path])
+            subprocess.run(["rm", delete_path])
+
+            print(f"Done {filename}.")
+            return True
+    except e:
+        return False
 
 
 def recompress_model_data():
